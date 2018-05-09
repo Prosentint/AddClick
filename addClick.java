102952a2d4a51ff46fa05b0011a21307212b5199
John Stephens <jdspokemon@gmail.com>
2018-05-09 15:15:04 -0400

    69

--- addClick.java
+++ addClick.java
@@ -1,9 +1,10 @@
-public class addClick
+public class addClick extends Application
 {
   public int CLICKER = 1;
   public int PERSEC = 0;
   public int TOTAL = 0;
   public int COUNTER = 0;
+  public int TIME = 0;
 
   public int ONEPCCOST = 20;
   public int FIVEPCCOST = 50
@@ -15,5 +16,17 @@ public class addClick
   public int TWOTSCOST = 400;
   public int FIVETSCOST = 1000;
 
-  
+  public int ADDCLICK = 0;
+  public int SUBCLICK = 0;
+  public int MULTICLICK = 0;
+  public int DIVICLICK = 0;
+
+  public int ADDSEC = 0;
+  public int SUBSEC = 0;
+  public int MULTISEC = 0;
+  public int DIVISEC = 0;
+
+  public int PURCHASECOUNT = 1;
+
+  public static
 }
